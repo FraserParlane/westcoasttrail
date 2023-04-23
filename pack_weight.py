3781950f86d961636f536f0cf505770c633cd17a
Fraser Parlane <mail@fraserparlane.com>
2023-04-23 15:03:17 -0700

    Sorting completed.

--- pack_weight.py
+++ pack_weight.py
@@ -1,17 +1,20 @@
+import plotly.express as px
 import pandas as pd
 import requests
 import os
 
 
-def google_sheet_to_csv():
+def google_sheet_to_csv(
+        filename: str,
+):
     """
     Download the Google Sheet with pack weight to disk.
+    :param filename: Filename to write to.
     :return: None
     """
 
     # Google Sheet URL
     url = 'https://docs.google.com/spreadsheets/d/1grJrvxfU2_ZW7QsBQBouVJazhB-X7JGaG9omMrKeNDE'
-    filename = 'weights.csv'
 
     # If old file exists, remove
     if os.path.exists(filename):
@@ -24,8 +27,65 @@ def google_sheet_to_csv():
         f.write(response.content)
 
 
+def get_weight_data(
+        cache: bool = True,
+        filename: str = 'weights.csv',
+) -> pd.DataFrame:
+    """
+    Get the weights.
+    :param cache: Don't download
+    :param filename: Filename to write/read to.
+    :return: pd.DataFrame
+    """
+    if not cache:
+        google_sheet_to_csv(filename=filename)
+
+    # Get data and return
+    df = pd.read_csv(filename)
+    return df
+
+
+def make_plot(
+        cache: bool = True,
+):
+    """
+    Make plot.
+    :param cache: Use cache.
+    :return: None
+    """
+
+    # Get data
+    df = get_weight_data(cache=cache)
+
+    # Common cols
+    mass = 'Total (g)'
+    cat = 'Category'
+
+    # Drop rows where no weight
+    df = df[~pd.isna(df[mass])]
+
+    # Drop rows where no category
+    df = df[~pd.isna(df[cat])]
+
+    # Get total weights by category, and sort
+    mass_sums = df.groupby(cat).sum()
+    cat_list = mass_sums.sort_values(by=mass, ascending=False)[mass].keys()
+    cat_order = pd.api.types.CategoricalDtype(cat_list)
+    df[cat] = df[cat].astype(cat_order)
+    df = df.sort_values(cat)
+
+    # Sort by cat, mass
+    df.sort_values(by=[cat, mass], ascending=[True, False], inplace=True)
+
+    # Get unique categories
+    categories = df[cat].unique()
+
+
+    print('a')
+    fig = px.bar(df, x=cat, y=mass)
+    fig.show()
 
 
 
 if __name__ == '__main__':
-    google_sheet_to_csv()
+    make_plot(cache=False)
